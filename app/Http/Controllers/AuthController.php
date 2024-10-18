17b1795f4eaf37feb4a0d34ff6cd4ccd356306bd
smmunna <minhazulabedinmunna@gmail.com>
2024-10-18 21:52:11 +0600

    project hasbeen initialized

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+   
+    // user registration 
+    public function registration(Request $request)
+    {
+        // Validation rules
+        $validator = Validator::make($request->all(), [
+            'name' => 'nullable|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:4',
+            'phone' => 'nullable|string|max:15|unique:users',
+            'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'address' => 'nullable|string|max:255',
+        ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // Create new user
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->phone = $request->phone;
+        $user->address = $request->address;
+
+        // Handle photo upload
+        if ($request->hasFile('photo')) {
+            $photo = $request->file('photo');
+
+            // Generate a unique ID for the file name
+            $uniqueId = uniqid();
+
+            // Get the current date and time
+            $currentDateTime = now()->format('Ymd_His');
+
+            // Get the original file name
+            $originalFileName = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
+            // Construct the new file name
+            $fileName = $originalFileName . '_' . $currentDateTime . '_' . $uniqueId . '.' . $photo->getClientOriginalExtension();
+            // Store the image in the storage directory with the constructed file name
+            $photoPath = $photo->storeAs('uploads/photos', $fileName, 'public');
+            // Save the photo path to the user model
+            $user->photo = $photoPath;
+        }
+
+        // Save user
+        $user->save();
+
+        return response()->json(['success' => true, 'message' =>'User saved successfully']);
+    }
+
+    //user login
+    public function login(Request $request)
+    {
+       
+    }
+
+}
